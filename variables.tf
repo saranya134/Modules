63c97023f9f43c50c01c2fa99f20a21be5fcce67
saranya134 <39684653+saranya134@users.noreply.github.com>
2022-07-15 11:01:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "eu-central-1"
+}
+ 
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type        = string
+  default     = "ami-065deacbcaac64cf2" # Amazon2 Linux AMI ID
+}
+ 
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 1
+}
+variable "itype" {
+  description = "instance type"
+  type        = string
+  default     = "t2.medium"
+}
+ variable "sg1" {
+  description = "instance type"
+  type        = string
+  default     = "sg1"
+}
+variable "sg2" {
+  description = "instance type"
+  type        = string
+  default     = "sg2"
+}
